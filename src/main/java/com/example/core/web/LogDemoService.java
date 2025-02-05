5bd16e01cd13b8433fb68efd148981797f45a684
jeonggiju <halogiju123@gmail.com>
2025-02-05 19:29:17 +0900

    2025/2/2

--- src/main/java/com/example/core/web/LogDemoService.java
+++ src/main/java/com/example/core/web/LogDemoService.java
@@ -1,8 +1,409 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N = int(input())                    # queuestack을 구성하는 N개의 자료구조
+A = list(map(int, input().split())) # 길이 N의 수열 A
+B = list(map(int, input().split())) # 길이 N의 수열 B
+M = int(input())                    # 삽입할 수열의 길이 M
+C = list(map(int, input().split())) # 길이 M의 수열 C
+
+queue = deque([])
+for i in range(N):
+        if A[i] == 0: # 큐인 자료구조만 모으기
+        queue.append(B[i])
+
+# 배열 C의 원소를 1개 appendleft 할 때마다 pop 연산 수행
+for i in range(M):
+        queue.appendleft(C[i])
+print(queue.pop(), end= ' ')package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
 package com.example.core.web;
 
 import com.example.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id){
+        myLogger.log("service id = " + id);
+    }
+}
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
